3ca5db66d451227e3fa07f60adef0eb0f59f833b
Andrew <mcdude22@gmail.com>
2024-01-24 08:49:09 -0800

    cleaned up code

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,49 @@
-import { createSignal, type Component, Show } from 'solid-js';
+import { createSignal, type Component, Show } from "solid-js";
 
-import styles from './App.module.css';
-import { state, setState } from './store';
+import styles from "./App.module.css";
+import { state, setState, tickSpeed, tick } from "./store";
 
-import Main from './main';
-import Test from './Test';
-
+import Main from "./main";
+import Test from "./Test";
 
 const App: Component = () => {
-    const timer = setInterval(()=> {
-        setState("bar", bar => bar + 1.0/state.tick);
-        if(state.bar>100){
-            setState("bar", 0.0);
-            if(state.action === 'Cycle'){
-                state.cycle.tick();
-            }else if(state.action === 'Train'){
-                state.train.tick();
-            }
-        }
-    }, 10);
+  const timer = setInterval(() => {
+    setState("bar", (bar) => bar + 1.0 / tickSpeed());
+    if (state.bar > 100) {
+      setState("bar", 0.0);
+      tick[state.action]();
+    }
+  }, 10);
 
   return (
     <div class={styles.App}>
-        <div class={styles.nav}>
-            <button class={styles.nav_item} onClick={() => {
-                    setState("menu", 'Main');
-            }}>Main</button>
-            <button class={styles.nav_item} onClick={() => {
-                setState('menu', 'Test');
-            }}>Test</button>
-        </div>
-        <progress max="100" value={state.bar}> </progress>
-        <Show when={state.menu === 'Main'}>
-            <Main/>
-        </Show>
-        <Show when={state.menu === 'Test'}>
-            <Test/>
-        </Show>
-
+      <div class={styles.nav}>
+        <button
+          class={styles.nav_item}
+          onClick={() => {
+            setState("menu", "Main");
+          }}
+        >
+          Main
+        </button>
+        <button
+          class={styles.nav_item}
+          onClick={() => {
+            setState("menu", "Test");
+          }}
+        >
+          Test
+        </button>
+      </div>
+      <progress max="100" value={state.bar}>
+        {" "}
+      </progress>
+      <Show when={state.menu === "Main"}>
+        <Main />
+      </Show>
+      <Show when={state.menu === "Test"}>
+        <Test />
+      </Show>
     </div>
   );
 };
