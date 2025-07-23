3b36de183388dd4e7531a6d5fca9ecface0c3efe
Andrew <mcdude22@gmail.com>
2025-07-23 10:06:26 -0700

    added box borders and custom font

---
+++ src/components/HealthBar.tsx
@@ -0,0 +1,11 @@
+import { type Component } from "solid-js";
+import utils from "../styles/utils.module.css";
+import { state, rankInfo } from "../state/store";
+
+export const HealthBar: Component = () => {
+	return (
+		<div>
+
+		</div>
+	)
+}
--- src/components/QuickInfo.tsx
+++ src/components/QuickInfo.tsx
@@ -5,6 +5,7 @@ import { state, rankInfo } from "../state/store";
 export const QuickInfo: Component = () => {
 	return (
 		<div class={utils.quickinfo}>
+			<p> HP: {state.health} </p>
 			<p>
 				Mana: {state.mana.toFixed(1)} of {state.maxMana.toFixed(1)}
 			</p>
@@ -12,7 +13,6 @@ export const QuickInfo: Component = () => {
 			<p> Action: {state.action} </p>
 			<p> Next Advancement: {rankInfo[state.rank].advMana} </p>
 			<p> Aspect: {state.aspect ? state.aspect : "Pure"} </p>
-			<p> HP: {state.health} </p>
 			<p> Coins: {state.coins} </p>
 			<p> State: {state.state} </p>
 			<p> Prev State: {state.previousState} </p>
